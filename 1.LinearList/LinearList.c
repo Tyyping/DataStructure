418002098969147772a146f990e1569599a690f4
Lon <wenlon7@outlook.com>
2019-04-11 11:37:58 +0800

    4.11 save

--- 1.LinearList/LinearList.c
+++ 1.LinearList/LinearList.c
@@ -1,6 +1,8 @@
 #include "LinearList.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
 #define LIST_INIT_LENGTH  20
 #define LIST_INCREASE     10
 
@@ -22,6 +24,7 @@ Status ListInit(SqList *L)
     L->listSize = LIST_INIT_LENGTH;
     return OK;
 }
+
 /*在指定位置插入一个元素， 0 <= pos <= L.length*/
 Status ListInsert(SqList *L, int pos, Stu stu)
 {
@@ -48,13 +51,14 @@ Status ListInsert(SqList *L, int pos, Stu stu)
     ++L->length;
     return OK;
 }
+
 /*删除指定位置上的元素，并将删除的元素存在stu中*/
-Status ListDelete(SqList *L, int pos, Stu *stu)
+Status ListDelete(SqList *L, int pos, Stu *pStu)
 {
     if(pos < 0 || pos >= L->length)
         return ERROR;
     //删除点数据删除
-    *stu = L->stu[pos];
+    *pStu = L->stu[pos];
     //删除点后面的数据，逐位向前移一位
     for(Stu * p = &L->stu[pos]; p <= &L->stu[L->length - 2]; p++)
     {
@@ -66,27 +70,81 @@ Status ListDelete(SqList *L, int pos, Stu *stu)
     return OK;
 }
 
+/*获取指定位置上的元素*/
+Status ListGetElem(SqList *L, int pos, Stu *pStu)
+{
+    //若给定位置不在顺序表的合法范围内，返回ERROR
+    if(pos < 0 || pos > L->length - 1)
+        return ERROR;
+    //将给定位置上的元素内容赋给*pStu
+    *pStu = L->stu[pos];
+    return OK;
+}
+
+/*比较x,y的值，相等时返回TRUE，不等返回FALSE*/
+static Status isEqual(Stu x, Stu y)
+{
+    if(x.number == y.number)
+    {
+        if(strcmp((const char *)x.name, (const char *)y.name) == 0)
+        {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+/*在顺序表中查找给定元素第一次出现的位置并返回，若顺序表中不存在该元素，则返回-1*/
+int ListLocate(SqList *L, Stu stu)
+{
+    //若顺序表长度小于等于0，表示顺序表内无任何元素
+    if(L->length <= 0)
+        return -1;
+    //从起始位置到终止位置，依次比较元素，当有相同元素出现时，返回元素位置，否则，返回-1
+    Stu *p = L->stu;
+    for(int cnt = 0; cnt < L->length; cnt++)
+    {
+        if(isEqual(stu, p[cnt]))
+            return cnt;
+    }
+    return -1;
+}
+
 Status main()
 {
-	getchar();
-
     SqList List;
     ListInit(&List);
 
     Stu stu;
-    stu.number = 1;
-    stu.name = "wenlon";
+    stu.number = 1; stu.name = "abc";
     ListInsert(&List, 0, stu);
 
-    stu.number = 2;
-    stu.name = "cheung";
+    stu.number = 2; stu.name = "def";
     ListInsert(&List, 1, stu);
 
-    stu.number = 3;
-    stu.name = "Lon";
-    ListInsert(&List, 1, stu);
+    stu.number = 3; stu.name = "ghi";
+    ListInsert(&List, 2, stu);
 
-    printf("%d\n", List.length);
+    stu.number = 4; stu.name = "jkl";
+    ListInsert(&List, 3, stu);
+
+    stu.number = 5; stu.name = "mn";
+    ListInsert(&List, 4, stu);
+
+    stu.number = 6; stu.name = "opq";
+    ListInsert(&List, 5, stu);
+
+    stu.number = 7; stu.name = "rst";
+    ListInsert(&List, 6, stu);
+
+    stu.number = 8; stu.name = "uvw";
+    ListInsert(&List, 7, stu);
+
+    stu.number = 9; stu.name = "xyz";
+    ListInsert(&List, 8, stu);
+
+    stu.number = 3; stu.name = "ghi";
+    printf("%d\n", ListLocate(&List, stu));
+
 
     ListDelete(&List, 0, &stu);
     printf("name=%s, number=%d\n", stu.name, stu.number);
@@ -96,6 +154,5 @@ Status main()
     printf("name=%s, number=%d\n", stu.name, stu.number);
     
 	getchar();
-    getchar();
 	
 }
